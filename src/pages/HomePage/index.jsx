19321205b8b01d40c86307c4abee0d1e92a090d2
s0lena <solena.work@gmail.com>
2024-06-24 07:30:45 +0300

    Replaced fetching room data from the api with import of the array from RoomData

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { Banner } from '../../components/Banner';
-import { RoomOrder } from '../../components/RoomOrder';
-import { Rooms } from '../../components/Rooms';
-import { Footer } from '../../components/Footer';
-import './style.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import { Banner } from "../../components/Banner";
+import { RoomOrder } from "../../components/RoomOrder";
+import { Rooms } from "../../components/Rooms";
+import { Footer } from "../../components/Footer";
+import "./style.css";
+import RoomData from "../../rooms";
 
 export const HomePage = () => {
   const [selectedRoom, setSelectedRoom] = useState(null);
-  const [roomList, setRoomList] = useState([]);
+  // const [roomList, setRoomList] = useState([]);
 
-  const selectedRoomData = roomList.find((room) => {
+  const selectedRoomData = RoomData.find((room) => {
     return room.name === selectedRoom;
   });
 
-  useEffect(() => {
-    const fetchRooms = async () => {
-      const response = await fetch('http://localhost:4000/api/rooms');
-      const data = await response.json();
-      console.log(data.data);
-      setRoomList(data.data);
-    };
-    fetchRooms();
-  }, []);
+  // useEffect(() => {
+  //   const fetchRooms = async () => {
+  //     const response = await fetch('http://localhost:4000/api/rooms');
+  //     const data = await response.json();
+  //     console.log(data.data);
+  //     setRoomList(data.data);
+  //   };
+  //   fetchRooms();
+  // }, []);
 
   return (
     <>
       <Banner />
-      <Rooms onSelect={setSelectedRoom} roomList={roomList} />
+      <Rooms onSelect={setSelectedRoom} roomList={RoomData} />
       {selectedRoomData && <RoomOrder selectedRoomData={selectedRoomData} />}
       <Footer />
     </>
